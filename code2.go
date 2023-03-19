34b57acca46553c5f60fabb1abad007501454010
Sabahat Shaik <93973063+SabaXs@users.noreply.github.com>
2023-03-19 10:09:31 +0530

    Create code2.go

---
+++ code2.go
@@ -0,0 +1,10 @@
+package main 
+import (
+  "fmt"
+  "math"
+)
+
+func main(){
+  fmt.Printf("The square root of 2 is %g.\n", math.Sqrt(2))
+  fmt.Printf("The answer of sin(45) by cos(45) is %.2f", math.Tan(45))
+}
