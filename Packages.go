2d7a3da5f6b4bf1f7413d4ba035fb28e38c21e3e
Sabahat Shaik <93973063+SabaXs@users.noreply.github.com>
2023-03-19 09:47:42 +0530

    Create Packages.go

---
+++ Packages.go
@@ -0,0 +1,13 @@
+package main
+import (
+  "fmt"
+  "math/rand"
+)
+
+func main(){
+  fmt.Println("The random number is ", rand.Intn(10))
+  a:= 4
+  fmt.Printf("My favourite number is %v \n", a)
+  var b string = "Sabahat"
+  fmt.Printf("Hello, I am %v ", b)
+}
